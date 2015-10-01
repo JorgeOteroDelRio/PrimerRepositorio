4dda4563209eea5c4e527e8c5b458fd7f6fa0fdd
joterodelrio <joterodelrio@atanasoff16.danielcastelao.org>
2015-10-01 08:45:20 +0200

    Inicio

---
+++ src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,19 @@
+
+package ejemplo2;
+
+
+public class Ejemplo2 {
+    static int num2; //variable de clase
+    
+    public static void main(String[] args) {
+        
+        int valor = 3, resultado; //varibles locales
+        
+        resultado = valor++;
+        System.out.println(resultado);
+        resultado = ++valor;
+        System.out.println(resultado);
+        
+    }
+    
+}
